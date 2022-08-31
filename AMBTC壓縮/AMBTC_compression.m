0f3f1a75435adb09d5325b9c1166d830751631b5
irene0516 <56101736+irene0516@users.noreply.github.com>
2022-08-31 22:33:35 +0800

    Update AMBTC_compression.m

--- AMBTC壓縮/AMBTC_compression.m
+++ AMBTC壓縮/AMBTC_compression.m
@@ -1,16 +1,13 @@
 training={'512 grayscale/lena.tif'};
 for kk=1:size(training,2)
-     org_img = imread(training{kk}); %­ì©l¹Ï
+     org_img = imread(training{kk}); %åŸå§‹åœ–
         % org_img=[44 83 84 44;43 83 42 44;42 82 82 81;42 82 81 80];
         [high,weight] = size(org_img(:,:,1));
         cover_img = org_img;
 for i=1:floor(high/4)
             for j=1:floor(weight/4)
-                origin = double(cover_img(((i-1)*4+1):i*4,((j-1)*4+1):j*4,1)); %±q¹Ï¤ù¤¤¨ú¥X3*3¤E®c®æblock
-                %                 origin=[52,53,53,54;45,43,55,49;57,46,58,63;58,59,59,63];
-                %   origin=[182,203,206,197;173,200,203,194;177,197,195,185;188,203,194,180];
+                origin = double(cover_img(((i-1)*4+1):i*4,((j-1)*4+1):j*4,1)); %å¾åœ–ç‰‡ä¸­å–å‡º3*3ä¹å®®æ ¼block
                 pic_mean=mean(origin,'all');
-                %            bitmap(origin<pic_mean)=0;
                 bitmap=zeros(4,4);
                 bitmap(origin>=pic_mean)=1;
                 bitmap3=bitmap;
